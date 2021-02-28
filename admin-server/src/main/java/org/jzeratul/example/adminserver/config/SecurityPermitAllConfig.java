b10d5034ee2df06e53c6ef2e141aba0d208b71a7
Vlad Vesa <vlad.vesa@gmail.com>
2021-02-28 21:38:53 +0100

    playing around with configs

---
+++ admin-server/src/main/java/org/jzeratul/example/adminserver/config/SecurityPermitAllConfig.java
@@ -0,0 +1,17 @@
+package org.jzeratul.example.adminserver.config;
+
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+//import org.springframework.context.annotation.Configuration;
+
+
+//@Configuration
+//public class SecurityPermitAllConfig` extends WebSecurityConfigurerAdapter` {
+//  @Override
+//  protected void configure(HttpSecurity http) throws Exception {
+//    http.authorizeRequests().anyRequest().permitAll()
+//            .and().csrf().disable();
+//  }
+//}
--- training-service1/src/main/java/org/jzeratul/example/service1/config/Service1Properties.java
+++ training-service1/src/main/java/org/jzeratul/example/service1/config/Service1Properties.java
@@ -3,10 +3,12 @@ package org.jzeratul.example.service1.config;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
-@ConfigurationProperties("funny")
 @Data
+@RefreshScope
 @NoArgsConstructor
+@ConfigurationProperties("funny")
 public class Service1Properties {
 
   private String message;
