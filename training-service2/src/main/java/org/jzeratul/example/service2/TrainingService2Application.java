1e8f4f86c81e9d0351f41f0d886fdb0a68c80018
Vlad Vesa <vlad.vesa@gmail.com>
2021-03-03 20:34:55 +0100

    various updates

--- training-service2/src/main/java/org/jzeratul/example/service2/TrainingService2Application.java
+++ training-service2/src/main/java/org/jzeratul/example/service2/TrainingService2Application.java
@@ -2,8 +2,12 @@ package org.jzeratul.example.service2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigurationProperties
 public class TrainingService2Application {
 
   public static void main(String[] args) {
